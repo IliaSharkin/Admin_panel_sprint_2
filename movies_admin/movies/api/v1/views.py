efe355b98d2606cbb754cbce92ae6486a6ffabe1
IliaSharkin <python3633@gmail.com>
2022-07-17 21:36:18 +0300

    add deteilview to views

--- movies_admin/movies/api/v1/views.py
+++ movies_admin/movies/api/v1/views.py
@@ -2,10 +2,11 @@ from django.contrib.postgres.aggregates import ArrayAgg
 from django.db.models import Q
 from django.http import JsonResponse
 from django.views.generic.list import BaseListView
+from django.views.generic.detail import BaseDetailView
 from movies.models import Filmwork
 
 
-class MoviesListApi(BaseListView):
+class MoviesApiMixin:
     model = Filmwork
     http_method_names = ['get']  # Список методов, которые реализует обработчик
     paginate_by = 50
@@ -33,17 +34,33 @@ class MoviesListApi(BaseListView):
                 distinct=True,
             ),
         )
+    
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
+    
+
+class MoviesListApi(MoviesApiMixin, BaseListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         queryset = self.get_queryset()
-        page_size = self.paginate_by
         paginator, page, queryset, is_paginated = self.paginate_queryset(
             queryset,
             self.paginate_by
         )
         return {
+            "count": paginator.count,
+            "total_pages": paginator.num_pages,
+            "prev": page.previous_page_number() if page.has_previous() else None,
+            "next": page.next_page_number() if page.has_next() else None,
             'results': list(queryset),
         }
 
-    def render_to_response(self, context, **response_kwargs):
-        return JsonResponse(context)
+
+class MoviesDetailApi(MoviesApiMixin, BaseDetailView):
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        pk = self.get_object()["id"]
+        context = super().get_context_data(kwargs={"pk": pk})
+        context = context["object"]
+        
+        return context
